5bfd40c67380f477b5cfe7eea44fa075a5215f92
pikipity <wangze19910407@gmail.com>
2021-05-10 11:11:50 +0800

    Delete test files

--- Test_receving_marker.m
+++
@@ -1,7 +0,0 @@
-clear;
-lib=lsl_loadlib();
-result = lsl_resolve_byprop(lib,'type','Markers');
-MarkerInletStreamInfo=result{1};
-MarkerInlet=lsl_inlet(MarkerInletStreamInfo);
-MarkerInlet.open_stream(2);
-[marker_chunk,marker_time]=MarkerInlet.pull_sample(0 );
